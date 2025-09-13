532fd1d99622e826830710ab73af268164d619b5
vishal27 <vishalkumargodia@gmail.com>
2025-09-13 09:33:05 +0530

    fix: corrected import path for InfoSection

--- src/View-trip/[tripId]/index.jsx
+++ src/View-trip/[tripId]/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { db } from '@/services/firebaseconfig';
 import { doc, getDoc } from 'firebase/firestore';
-import Infosection from '../components/infosection';
+import Infosection from '../components/Infosection';
 import { Hotel } from 'lucide-react';
 import Hotels from '../components/Hotels';
 import PlacesTovisit from '../components/PlacesToVisit';
